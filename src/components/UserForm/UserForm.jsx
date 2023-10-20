840541c9bfe66984102f7b8e5df4be3c255e59e2
CKirbs <ckggithub@gmail.com>
2023-10-20 20:51:48 +0800

    Added UserForm Component + CSS STyle

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -4,7 +4,23 @@ import Card from "../cards/card";
 const UserForm = () => {
   return (
     <>
-      <Card className={`flex-column`}></Card>
+      <form>
+        <Card className={`${style["form-card"]} flex-column`}>
+          <p>User Info Form</p>
+
+          <div className={`${style["form-component"]} flex`}>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" />
+          </div>
+
+          <div className={`${style["form-component"]} flex`}>
+            <label htmlFor="age">Age:</label>
+            <input type="number" id="age" />
+          </div>
+
+          <button type="submit">Submit</button>
+        </Card>
+      </form>
     </>
   );
 };
